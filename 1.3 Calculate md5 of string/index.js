8abc9feb3666bfc0167cc5b4b8bba96f15b1064e
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-03-25 22:48:43 +0200

    2 batch of comments

--- 1.3 Calculate md5 of string/index.js
+++ 1.3 Calculate md5 of string/index.js
@@ -1,16 +1,14 @@
-const fs = require("node:fs");
-// const md5 = require("md5");
 const crypto = require("node:crypto");
-
-const writeStream = fs.createWriteStream("write.txt");
 
 const arg = process.argv;
 const string = arg[2];
 
 try {
   let stringMD5 = crypto.createHash("md5").update(string).digest("hex");
-  writeStream.write(stringMD5);
+  console.log(stringMD5);
 } catch (err) {
-  writeStream.write("crypto support is disabled! \n");
-  writeStream.write(err.code);
+  process.on("exit", (code) => {
+    console.log(`About to exit with code: ${code}`);
+  });
+  process.exit(1);
 }
