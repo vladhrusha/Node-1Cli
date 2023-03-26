8ff2a8650dc9faaf4d2de802250473ab8ca4aa37
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-03-26 15:06:34 +0300

    task 1.4_md5OfFile

---
+++ 1.4_md5OfFile/index.js
@@ -0,0 +1,17 @@
+const util = require("util");
+const crypto = require("node:crypto");
+const readFile = util.promisify(require("node:fs").readFile);
+const readFilePromise = readFile("read.txt", "utf8");
+
+readFilePromise
+  .then((fileContent) => {
+    let md5String = crypto.createHash("md5").update(fileContent).digest("hex");
+    console.log(md5String);
+  })
+  .catch((err) => {
+    console.log(err.code);
+    process.on("exit", (code) => {
+      console.log(`About to exit with code: ${code}`);
+    });
+    process.exit(1);
+  });
