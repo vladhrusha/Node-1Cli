c099be478265702219e7488501d6b6ed1095a394
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-03-28 10:50:45 +0300

    fixing problems listed in batch of comments 2

--- 1.4_md5OfFile/index.js
+++ 1.4_md5OfFile/index.js
@@ -5,14 +5,10 @@ function getChecksum(path) {
   return new Promise(function (resolve, reject) {
     const hash = crypto.createHash("md5");
     const input = fs.createReadStream(path);
-    input.on("error", (err) => {
-      process.on("exit", (code) => {
-        console.log(`About to exit with code: ${code}`);
-        console.log(err);
-      });
-      process.exit(1);
-    });
 
+    input.on("error", (err) => {
+      reject(err);
+    });
     input.on("data", function (chunk) {
       hash.update(chunk);
     });
@@ -23,8 +19,14 @@ function getChecksum(path) {
   });
 }
 
-Promise.all([getChecksum("read.txt"), getChecksum("12,5GB.mkv")]).then(
-  (values) => {
+Promise.all([getChecksum("read.txt"), getChecksum("12,5GB.mkv")])
+  .then((values) => {
     console.log(values);
-  }
-);
+  })
+  .catch((err) => {
+    process.on("exit", (code) => {
+      console.log(`About to exit with code: ${code}`);
+      console.log(err);
+    });
+    process.exit(1);
+  });
