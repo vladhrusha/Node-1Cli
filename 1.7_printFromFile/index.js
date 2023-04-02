e06028bedfab558bbaf9888cace35ac9fda61c22
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-02 11:56:46 +0300

    for await implementation

--- 1.7_printFromFile/index.js
+++ 1.7_printFromFile/index.js
@@ -3,8 +3,8 @@ const fs = require("node:fs");
 const readline = require("node:readline/promises");
 const { output } = require("node:process");
 
-function countCountries(path) {
-  return new Promise(function (resolve, reject) {
+async function countCountries(path) {
+  return new Promise(async function (resolve, reject) {
     const input = fs.createReadStream(path);
     const rl = readline.createInterface({ input, output });
     const countriesCounter = new Map();
@@ -13,21 +13,24 @@ function countCountries(path) {
       reject(err);
     });
 
-    rl.on("line", function (chunk) {
-      let index = chunk.indexOf(",");
-      let country = chunk.slice(0, index);
+    for await (const line of rl) {
+      let index = line.indexOf(",");
+      let country = line.slice(0, index);
       countriesCounter.set(country, countriesCounter.get(country) + 1 || 1);
-    });
+    }
 
     input.on("close", function () {
       resolve(countriesCounter);
     });
   });
 }
-
+const startTimestamp = Date.now();
 countCountries("free_company_dataset.csv")
   .then((values) => {
     console.log(values);
+    const endTimestamp = Date.now();
+    const secondsElapsed = Math.round((endTimestamp - startTimestamp) / 1000);
+    console.log(`Stopped after ${secondsElapsed} seconds`);
   })
   .catch((err) => {
     process.on("exit", (code) => {
