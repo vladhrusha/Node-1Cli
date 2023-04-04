07b0aac9dd2b54f3503ae63184ddeac056b5fc2f
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-04 17:47:54 +0300

    task 1.8_showpercentage

---
+++ 1.8_showCompletionPercentage/README.md
@@ -0,0 +1,10 @@
+### Task:
+
+    The processing is long and user doesn't understand how long he needs to wait.
+    Add cli-progress package for better user experience. Think how you calculate process.
+
+#### Usage instruction
+
+    1. navigate into "1.8_showCompletionPercentage" directory in your terminal
+    2. run the command "npm install" to install required package
+    3. use the command "node index.js"
---
+++ 1.8_showCompletionPercentage/index.js
@@ -0,0 +1,63 @@
+const fs = require("node:fs");
+const cliProgress = require("cli-progress");
+
+const readline = require("node:readline/promises");
+const { output } = require("node:process");
+
+function countCountries(path) {
+  const bar1 = new cliProgress.SingleBar(
+    {
+      format:
+        "CLI Progress |" +
+        "{bar}" +
+        "| {percentage}% | {value}/{total} megabytes",
+    },
+    cliProgress.Presets.shades_classic
+  );
+
+  return new Promise(async function (resolve, reject) {
+    const input = fs.createReadStream(path);
+    const rl = readline.createInterface({ input, output });
+    const countriesCounter = new Map();
+    let barCounter = 0;
+
+    var fileSizeInMegaBytes = Math.round(fs.statSync(path).size / 1024 / 1024);
+    bar1.start(fileSizeInMegaBytes, barCounter);
+
+    input.on("error", (err) => {
+      reject(err);
+    });
+    input.on("data", (chunk) => {
+      barCounter = barCounter + Buffer.byteLength(chunk, "utf8");
+      bar1.update(Math.round(barCounter / 1024 / 1024));
+    });
+
+    for await (const line of rl) {
+      let country = line.slice(0, line.indexOf(","));
+      countriesCounter.set(country, countriesCounter.get(country) + 1 || 1);
+    }
+
+    input.on("close", function () {
+      countriesCounter.delete('""');
+      countriesCounter.delete("country");
+      bar1.stop();
+      resolve(countriesCounter);
+    });
+  });
+}
+const startTimestamp = Date.now();
+countCountries("free_company_dataset.csv")
+  .then((values) => {
+    console.log(values);
+    const endTimestamp = Date.now();
+    const secondsElapsed = Math.round((endTimestamp - startTimestamp) / 1000);
+    console.log(`Stopped after ${secondsElapsed} seconds`);
+  })
+  .catch((err) => {
+    process.on("exit", (code) => {
+      console.log(`About to exit with code: ${code}`);
+    });
+    process.exit(1);
+  });
+
+//cd 1.8_showCompletionPercentage
